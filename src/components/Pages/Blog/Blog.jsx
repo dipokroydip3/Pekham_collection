e83e53040d0cc3a271a30456c4ba73312fb78db3
Dipak Roy <dipokroydip3@gmail.com>
2025-01-18 04:14:08 +0600

    some update in blog page

--- src/components/Pages/Blog/Blog.jsx
+++ src/components/Pages/Blog/Blog.jsx
@@ -3,7 +3,7 @@
 const Blog = () => {
       return (
             <div className="my-10 w-11/12 m-auto">
-                  This is blog page
+                  This is blog pages at the some time 
             </div>
       );
 };
