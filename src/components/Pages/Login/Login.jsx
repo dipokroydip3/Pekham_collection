6000b5e1ebf13e9065947f3f4ad714d78f64657d
Dipak Roy <dipokroydip3@gmail.com>
2025-03-21 22:55:03 +0600

    some added in login page

--- src/components/Pages/Login/Login.jsx
+++ src/components/Pages/Login/Login.jsx
@@ -2,15 +2,16 @@
 
 const Login = () => {
       return (
-            <div className="my-10 w-11/12 m-auto">
-                  This is login page
+            <div className="my-10 w-full mx-0 m-auto">
+                  <h3 className="text-center my-10">This is login page</h3>
 
-                  <div className="w-6/12 space-y-10 inline-block ml-300 m-auto mx-0">
-                        <input className="block bg-gray-300 w-full py-3 px-8 rounded" type="email" placeholder="Email" />
-                        <input className="block bg-gray-300 w-full py-3 px-8 rounded" type="password" placeholder="Password" />
-                        <button className="bg-blue-700 rounded text-white font-semibold px-10 py-3">Login</button>
-                        
-                  </div> 
+                  <div className="flex justify-center items-center m-auto ">
+                        <div className=" border w-5/12 space-y-10">
+                              <input className="block bg-gray-300 w-full py-3 px-8 rounded" type="email" placeholder="Email" />
+                              <input className="block bg-gray-300 w-full py-3 px-8 rounded" type="password" placeholder="Password" />
+                              <button className="bg-blue-700 block m-auto rounded text-white font-semibold px-10 py-3">Login</button>
+                        </div>
+                  </div>
             </div>
       );
 };
