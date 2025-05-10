3da0f9eed5d3635419c9b767c1c46b1cec989c4e
dipokroydip3 <dipokroydip3@gmail.com>
2025-05-10 14:12:42 +0600

    some update

--- src/components/Pages/Shop/Shop.jsx
+++ src/components/Pages/Shop/Shop.jsx
@@ -3,7 +3,7 @@
 const Shop = () => {
       return (
             <div className="my-10 w-11/12 m-auto">
-                  This is shop page
+                  This is shop page, this is the shope page
             </div>
       );
 };
